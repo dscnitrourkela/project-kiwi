9247ea681f6824d73fb60f46a071058239fc7808
Sambit_Sankalp <sambit.sankalp.official@gmail.com>
2021-09-03 20:08:16 +0530

    init:gatsby configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Project Kiwi</div>
-}
+const Home = () => {
+  return <div>Project Kiwi</div>;
+};
+
+export default Home;
