78f6cff3c7d0d6500aa14f75360287afdd508634
Sambit Sankalp <82284130+sambit-sankalp@users.noreply.github.com>
2021-09-11 18:22:01 +0530

    init: Setup Dependencies, Project Structure, and README (#2)
    * init: initial files added
    
    * init:gatsby configuration
    
    * fix:images added and md files updated
    
    * init:github workflows
    
    * Minor Fixes
    
    * remove lighthouse.yml
    
    Co-authored-by: CIPHERTron <pritish.samal918@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  pathPrefix: `/`,
+  siteMetadata: {
+    title: `Leaderboard`,
+    siteUrl: `https://leaderboard.dscnitrourkela.org`,
+    description: `Leaderboard of HackNITR 3.0`,
+    canonicalUrl: `https://leaderboard.dscnitrourkela.org`,
+    author: {
+      name: `Leaderboard`,
+      description: `Leaderboard of HackNITR 3.0`,
+    },
+    organisation: {
+      name: `DSC NIT Rourkela`,
+      url: `https://leaderboard.dscnitrourkela.org`,
+    },
+  },
+  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-robots-txt`],
+};
